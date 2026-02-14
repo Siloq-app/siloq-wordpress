4da0dd58e90e40318426576ff74451c3afd9c777
Arc (Siloq AI) <arc@siloq.ai>
2026-02-14 16:31:05 -0600

    bump version to 1.4.6

--- siloq-connector/siloq-connector.php
+++ siloq-connector/siloq-connector.php
@@ -3,7 +3,7 @@
  * Plugin Name: Siloq Connector
  * Plugin URI: https://github.com/Siloq-seo/siloq-wordpress-plugin
  * Description: Connects WordPress to Siloq platform for SEO content silo management and AI-powered content generation
- * Version: 1.4.5
+ * Version: 1.4.6
  * Author: Siloq
  * Author URI: https://siloq.com
  * License: GPL v2 or later
